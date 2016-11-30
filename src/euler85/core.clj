f5d510c72b26c14359929878e8c9f86b16522bc9
Andrew Sinclair <andrew.george.sinclair@gmail.com>
2016-11-29 22:49:45 -0600

    works now

--- src/euler85/core.clj
+++ src/euler85/core.clj
@@ -31,17 +31,17 @@
 (defn calc-column
   [i]
   (let [iC2 (nChoose2 i)
-        start-num (square (nChoose2 (dec i)))]
+        start-num (square (nChoose2 i))]
     (->>
-      (iterate inc start-num)
-      (reductions (fn [prev j] (+ iC2 prev))))))
+      (iterate inc i)
+      (reductions (fn [prev j] (+ prev (* (inc j) iC2))) start-num))))
 
 (defn last-2-while-less-than
   [max-total xs]
   (->>
     xs
     (tuplize 2)
-    (drop-while #(< (first %) max-total))
+    (drop-while #(< (second %) max-total))
     first))
 
 (defn distance-from
@@ -56,20 +56,26 @@
     (map (partial distance-from max-total))
     (apply min)))
 
-
 (defn calc-euler-85
   [max-total]
   (let [max-square-size (analyze-square-size max-total)
         indexed-columns (->>
                           (range 1 (inc max-square-size))
                           (map calc-column)
-                          (map-indexed cons))
+                          (map-indexed #(cons (inc %1) %2)))
         column-j        (apply min-key (partial min-column-fn max-total) indexed-columns)
-		trimmed-column  (take-while #(< % max-total) column-j)]
-    [(first trimmed-column) (count trimmed-column)]))
+        trimmed-column  (take-while #(< % max-total) column-j)
+        min-val         (apply min-key (partial distance-from max-total) trimmed-column)]
+    [ (first column-j)
+      (->>
+        column-j
+        (drop 1)
+        (take-while #(< % min-val))
+        count
+        (+ (first column-j)))]))
 
 (defn -main
   "Euler 85"
   [& args]
-  (let [max-total 20000000]
+  (let [max-total 2000000]
     (println (calc-euler-85 max-total))))
